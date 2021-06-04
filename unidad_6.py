f1afcde30e9340d3944caef7798a342e5c83af71
paladinescamila <camilapaladines27@gmail.com>
2021-06-04 17:44:55 -0500

    Beautify methods code

--- unidad_6.py
+++ unidad_6.py
@@ -24,7 +24,7 @@ def euler(ODE, t0, y0, tf, h):
     tk, yk = pasos[-1]
 
     while (tk < tf):
-        yk += f(tk, yk)*h
+        yk += f(tk, yk) * h
         tk += h
         pasos.append((tk, yk))
     
@@ -73,8 +73,8 @@ def runge_kutta_2(ODE, t0, y0, tf, h):
     tk, yk = pasos[-1]
 
     while (tk < tf):
-        k1 = f(tk, yk)*h
-        k2 = f(tk + h, yk + k1)*h
+        k1 = f(tk, yk) * h
+        k2 = f(tk + h, yk + k1) * h
         yk += (1/2) * (k1 + k2)
         tk += h
         pasos.append((tk, yk))
@@ -98,10 +98,10 @@ def runge_kutta_4(ODE, t0, y0, tf, h):
     tk, yk = pasos[-1]
 
     while (tk < tf):
-        k1 = f(tk, yk)*h
-        k2 = f(tk + h/2, yk + k1/2)*h
-        k3 = f(tk + h/2, yk + k2/2)*h
-        k4 = f(tk + h, yk + k3)*h
+        k1 = f(tk, yk) * h
+        k2 = f(tk + h/2, yk + k1/2) * h
+        k3 = f(tk + h/2, yk + k2/2) * h
+        k4 = f(tk + h, yk + k3) * h
         yk += (1/6) * (k1 + 2*k2 + 2*k3 + k4)
         tk += h
         pasos.append((tk, yk))
@@ -146,8 +146,7 @@ def multipaso_4(ODE, t0, y0, tf, h):
     """
 
     f = sym.lambdify([t, y], ODE)
-    [(t1, y1), (t2, y2), (t3, y3)] = runge_kutta_4(ODE, t0, y0, t0+3*h, h)[1:4]
-    pasos = [(t0, y0), (t1, y1), (t2, y2), (t3, y3)]
+    pasos = [(t0, y0)] + runge_kutta_4(ODE, t0, y0, t0 + 3*h, h)[1:4]
     tk, yk = pasos[-1]
 
     while (tk < tf):
@@ -177,8 +176,8 @@ def ODEs_superior(ODE, t0, y0s, tf, h, n):
 
     while (tk < tf):
         f = sym.lambdify([t, y], ODE)
-        for i in range(n-1, -1, -1):
-            yk[i] += f(tk, yk[i])*h
+        for i in range(n - 1, -1, -1):
+            yk[i] += f(tk, yk[i]) * h
             f = sym.lambdify([t, y], yk[i])
         tk += h
         pasos.append((tk, yk[0]))
@@ -202,7 +201,7 @@ def diferencias_finitas(ODE, t0, y0, tf, yf, n):
     h = (tf - t0) / (n - 1)
 
     A = [[0 for _ in range(n - 2)] for _ in range(n - 2)]
-    b = [f(ti[i])*h**2 for i in range(1, n - 1)]
+    b = [f(ti[i]) * (h**2) for i in range(1, n - 1)]
     
     for i in range(1, n - 1):
 
@@ -211,8 +210,8 @@ def diferencias_finitas(ODE, t0, y0, tf, yf, n):
 
         if (i == 1): b[j] -= y0
         if (i == n - 2): b[j] -= yf
-        if (i > 1): A[j][j-1] = 1
-        if (i < n-2): A[j][j+1] = 1
+        if (i > 1): A[j][j - 1] = 1
+        if (i < n - 2): A[j][j + 1] = 1
     
     yi = [y0] + list (np.linalg.solve(A, b)) + [yf]
     pasos = [(ti[i], yi[i]) for i in range(n)]
@@ -245,9 +244,9 @@ def elementos_finitos(ODE, t0, y0, tf, yf, n):
             b.append(yf)
 
         else:
-            A.append([j*(j-1)*ti[i]**(j-2) for j in range(n)])
+            A.append([j * (j - 1) * ti[i]**(j - 2) for j in range(n)])
             b.append(f(ti[i]))
 
-    coef = np.linalg.solve(A, b)
-    polinomio = sum([coef[i]*t**i for i in range(n)])
+    x = np.linalg.solve(A, b)
+    polinomio = sum([x[i] * (t**i) for i in range(n)])
     return polinomio
